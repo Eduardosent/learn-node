818f7ef306ecd1c36c05456f24ac27e4274914e7
Eduardo <eduardogomez.sent2@gmail.com>
2024-05-09 18:42:42 -0500

    fix: change react word

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,7 @@ import Layout from "../../layout/Layout";
 export default function Home(){
     return(
         <Layout>
-            <Title>Bienvenido a LearnEdu, aprende React</Title>
+            <Title>Bienvenido a LearnEdu, aprende Node</Title>
             <TitleDescription>LearnEdu es un proyecto donde podrás aprender y perfeccionar Tecnologías, las páginas se actualizarán constantemente para que puedas mejorar tu aprendizaje constantemente.</TitleDescription>
             <SubTitle>Lista de secciones para aprender React</SubTitle>
             <PageList/>
